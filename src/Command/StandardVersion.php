b0b751a9c10f39cc1637a4bf480f455e1096f728
Rafael Z. Kineippe <rafael@zamana.com.br>
2018-05-05 16:51:44 -0300

    Cleaning Up

---
+++ src/Command/StandardVersion.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * (c) Rafael Z. Kineippe <rafael@zamana.com.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * PHP Version 7
+ *
+ * @category Principal
+ * @package  StandardVersion
+ * @author   Rafael Z. Kineippe <rafael@zamana.com.br>
+ * @license  https://opensource.org/licenses/ISC ISC
+ * @link     https://github.com/rzamana/standard-version
+ */
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command for symfony console
+ *
+ * Register the basic command line operations that we want!
+ */
+class StandardVersion extends Command
+{
+    protected function configure() 
+    {
+    }
+
+    protected function execute( InputInterface $input, OutputInterface $output ) 
+    {
+    }
+}
