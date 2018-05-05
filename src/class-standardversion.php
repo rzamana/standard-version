e3332c64ff58d6f98f2214e6040fe982ba02bfb0
Rafael Z. Kineippe <rafael@zamana.com.br>
2018-05-05 12:30:35 -0300

    style: Starting the basic structure

---
+++ src/class-standardversion.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of rzamana/standard-version.
+ *
+ * (c) Rafael Z. Kineippe <rafael@zamana.com.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package StandardVersion
+ * @author Rafael Z. Kineippe <rafael@zamana.com.br>
+ */
+
+namespace StandardVersion;
+
+/**
+ * StandardVersion Class
+ */
+final class StandardVersion {
+}
